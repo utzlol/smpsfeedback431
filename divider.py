e6757ceada48bebfe3ec8867002f59cc0461ba4c
utzlol <40377777+utzlol@users.noreply.github.com>
2022-12-04 22:20:19 +0700

    Update divider.py

--- divider.py
+++ divider.py
@@ -1,30 +1,60 @@
 from sympy.solvers import solve
 from sympy import Symbol
-x = Symbol('x')
-a = 1
+import time
 
+x = Symbol('x')
+vc = 0
+a = 1
+done = 0
+radd = 0
+vtot = 0
 def p(x,y):
 	a = float(x)*float(y)/(float(x)+float(y))
 	return float(a)
 
-v = input("volt : ")
-rtot = input("rtot : ")
-rout = input("rout : ")
+def getp(x):
+	while "p" in str(x):
+		xa = str(x).replace('p','iniganti',1).split("iniganti")[1]
+		xa = xa.replace(")","iniganti",1).split("iniganti")[0]
+		xa = xa.replace("(","").split(",")
+		val = p(xa[0], xa[1])
+		x = x.replace(f"p({str(xa[0])},{str(xa[1])})", str(val))
+		x = eval(x)
+	x = eval(str(x))
+	return float(x)
 
-while "p" in str(rtot):
-	rtota = str(rtot).replace('p','iniganti',1).split("iniganti")[1]
-	rtota = rtota.replace(")","iniganti",1).split("iniganti")[0]
-	rtota = rtota.replace("(","").split(",")
-	val = p(rtota[0], rtota[1])
-	rtot = rtot.replace(f"p({str(rtota[0])},{str(rtota[1])})", str(val))
-	rtot = eval(rtot)
 
-print(rtot)
 
-v = float(v)
-rtot = float(rtot)
-rout = float(rout)
+#v = input("volt : ")
+#rtot = input("rtot : ")
+rtot = "p(20,510) + p(75,1.05) + 3.83"
+rtot = getp(rtot)
+#rout = input("rout : ")
+rout = "p(20,510)"
+rout = getp(rout)
+rexcp = rtot - rout
+#v = float(v)
 
-itot = v/rtot
-vout = v - rout*itot
-print(vout)
+#itot = v/rtot
+#vout = v - rout*itot
+#print(vout)
+
+while done != 1:
+#while "15.00" not in str(vtot):
+#while True:
+	routc = rout + radd
+	#routc = p(rout,radd)
+	rtotc = rexcp + routc
+#	ic = v/rtotc
+#	vc = v - ic*routc
+	vtot = 2.5/(rexcp)*rtotc
+	if radd > 20:
+		done = 1
+	radd = radd + 0.01
+	#print(f"Vout : {vc}")
+	print(vtot)
+	print(f"Radd : {radd}")
+	#print(rexcp)
+	print(f"Rtotal : {rtotc}")
+	print(f"Rout : {routc}")
+	#time.sleep(0.1)
